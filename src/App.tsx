bdee8cb17991bb8d1b9910debe01ebd7d9c68518
Leslie Xin <leslie.xin@shopify.com>
2020-10-25 01:16:15 -0400

    styled slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
 import React from 'react';
-import './App.css';
 import Home from './components/Home';
 
 function App() {
   return (
     <Home/>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,4 +1,40 @@
 import React, {useState} from 'react';
+import styled from 'styled-components';
+import slide from "../assets/slide.png";
+import thumb from "../assets/thumb.png";
+
+const StyledSlider = styled.div`
+    z-index: 1;
+    width: 250px;
+
+    .slider {
+        width: 250px;
+        height: auto;
+        -webkit-appearance: none;
+        border-radius: 5px;
+        outline: none;
+        background: url(${slide}) no-repeat;
+        background-size: 250px;
+
+
+        &::-webkit-slider-thumb {
+            -webkit-appearance: none;
+            appearance: none;
+            width: 35px;
+            height: 35px;
+            cursor: pointer;
+            background: url(${thumb}) no-repeat;
+            background-size: 35px;
+        }
+
+        &::-moz-range-thumb {
+            width: 20px;
+            height: 20px;
+            cursor: pointer;
+            outline: 2px solid #333;
+        }
+    }
+`
 
 interface Props {
     id: string
@@ -12,21 +48,18 @@ function Slider({id}: Props) {
     };
 
     return (
-        <div style={{
-            zIndex: 1,
-        }}>
-            <label>
-                <input
-                    id={id}
-                    type="range" 
-                    min="0"
-                    max="5" 
-                    step="1"
-                    value={slideValue} 
-                    onChange={handleChange}
-                />
-            </label>
-        </div>
+        <StyledSlider>
+            <input
+                id={id}
+                type="range" 
+                min="0"
+                max="1" 
+                step="0.05"
+                value={slideValue} 
+                onChange={handleChange}
+                className="slider"
+            />
+        </StyledSlider>
     );
 }
 
