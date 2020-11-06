41ba4fa844de9550c4a4b792fd2acc76d7763510
Leslie Xin <leslie.xin@shopify.com>
2020-11-05 21:04:16 -0500

    GA

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ReactGA from 'react-ga';
 import Home from './components/Home';
 
 function initializeReactGA() {
-  ReactGA.initialize('G-8CRH8MM291');
+  ReactGA.initialize('UA-150454598-2');
   ReactGA.pageview('/homepage');
 }
 
